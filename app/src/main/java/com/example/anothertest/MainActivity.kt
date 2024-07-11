b4d92fc05f35783d4209587a426acaf95bdf2f9e
Eli Gabriel Soriano <elta.soriano.up@phinmaed.com>
2024-07-11 19:22:41 +0800

    Welcome and Register

---
+++ app/src/main/java/com/example/anothertest/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.anothertest
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.anothertest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        binding.registerBtn.setOnClickListener {
+            val intent = Intent(this, SignupPage::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/anothertest/SignupPage.kt
@@ -0,0 +1,13 @@
+package com.example.anothertest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SignupPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_page)
+
+    }
+}
